257bc06d01a25743341d430e5a8e061f3b476b60
N0TaN1CK <nickich127@gmail.com>
2023-06-19 23:47:13 +0300

    типа финалочка

--- Frontend.py
+++ Frontend.py
@@ -1,49 +1,84 @@
 import streamlit as st
-import numpy
 import altair
 import pandas as pd
-from scipy.integrate import solve_ivp as solve
-from chislenno import solve_equation
-from chislenno import parse_equation
+from sympy import simplify,re
+from sympy.abc import t
+from Mid2 import solve_normally
 
 st.title("Решатель дифференциальных уравнений")
-want_solution = st.checkbox('с решением')
-if want_solution:
-     st.subheader("Введите уравнение:")
-     st.text_input("", key="input")
-     st.subheader("Введенное уравнение:")
-     st.latex(st.session_state.input)
-     st.subheader("Решение:")
-     st.write("Здесь потом будет решение")
-else:
-     st.subheader("Введите уравнение:")
-     eq = st.text_input("пример правильной записи: 4.7 * d^2y/dt^2 - 33.5 * dy/dt + 43.7 y  = 77.8")
-     st.subheader("Введите начальное значение y")
-     yst = st.text_input("",key="inputy")
-     st.subheader("Введите начальное значение dy/dt")
-     dydtst = st.text_input("", key="inputdydt")
-     st.subheader("Введите начало временного промежутка")
-     t1st = st.text_input("", key="inputt1")
-     st.subheader("Введите конец временного промежутка")
-     t2st = st.text_input("", key="inputt2")
-     ready = st.button("решить")
-     if ready:
-          yy = numpy.float64(yst)
-          dydt = numpy.float64(dydtst)
-          t1 = numpy.float64(t1st)
-          t2 = numpy.float64(t2st)
-          eqpar = parse_equation(eq)
-          sol = solve_equation(eqpar,t1,t2,dydt,yy)
-          i = 0
-          if eqpar[1]>=0:
-               i+=1
-          for_graph = {'t':list(sol[0]),'y':list(sol[1][i])}
-          data = pd.DataFrame(for_graph)
-          graph_spec = altair.Chart(data).mark_line(interpolate="monotone").encode(
-               x = "t",
-               y = "y"
-          )
-          graph = st.altair_chart(graph_spec,use_container_width=True)
+st.subheader("Введите уравнение:")
+eq = st.text_input("Вводите в LaTeX формате")
+counter = 0
+counter2 = 0
+followup = []
+for symbol in eq:
+    if symbol == '\\':
+        followup.append(eq[counter2 + 1:counter2 + 3])
+        counter += 1
+    counter2 += 1
+followup.append('sq')
+followup.append('ex')
+degree = 1
+eq2 = eq.replace("\\frac{d^2y}{dt^2}","ddf(tttttt)").replace("\\frac{dy}{dt}","df(tttttt)").replace("{","(").replace("}",")").replace("lambda","paskhalochka").replace("Lambda","PASKHALOCHKA").replace('e^','exp').replace('tau','STHONE').replace('theta','STHTWO').replace('beta','bbbbb').replace('gamma','ggggg').replace('zeta','zzzzz')
+if eq2.find('ddf(tttttt)')>=0:
+    degree = 2
+eq3 = eq2.replace('\\','').replace("y","f(tttttt)").split("=")
+if eq!='':
+    st.subheader("Вы ввели:")
+    st.latex(eq)
+    res = solve_normally('f','tttttt',eq3[0],eq3[1],degree).replace("PASKHALOCHKA","Lambda").replace("paskhalochka","lambda").replace('**','^').replace('tau','y(0)').replace('theta','\\frac{dy}{dt}(0)').replace('STHONE','tau').replace('STHTWO','theta').replace('bbbbb','beta').replace('ggggg','gamma').replace('zzzzz','zeta')
+    for item in followup:
+        for j in range(len(res)-1,0,-1):
+            if res[j:j+2]==item and res[j-1]!='\\':
+                res = res[0:j]+'\\'+res[j:len(res)+1]
+
+    st.subheader("Решение:")
+    st.latex(res.replace('tttttt','t'))
+    varis = []
+    do_not_touch = 'sqrtexpsincostancotanctantgarctgtttttt'
+    res2 = '%'+res.replace("\\","").replace('y(0)','yin').replace('frac{dy}{dt}(0)','dydtin').replace("^","**").split('=')[1]
+    coun = 1
+    letter = ''
+    for i in range(1,len(res2)-1):
+        if res2[i].isalpha() and not(res2[i-1].isalpha()):
+            letter = res2[i]
+            while letter.isalpha():
+                letter = res2[i:i+coun]
+                coun+=1
+            letter = letter[0:len(letter)-1]
+            if do_not_touch.find(letter)<0:
+                varis.append(letter)
+                do_not_touch=do_not_touch+letter
+        letter = ''
+        coun = 1
+    nums = []
+    sl = []
+    st.subheader("Задайте переменные")
+    for j in range(len(varis)):
+        sl.append(1)
+        sl[j] = st.slider('$'+varis[j].replace('yin','y(0)').replace('dydtin','\\frac{dy}{dt}(0)')+'$',min_value=-100)
+        nums.append(float(sl[j]))
+    for k in range(len(varis)):
+        res2 = res2.replace('%','').replace(varis[k],str(nums[k]))
+    t0 = st.slider("Начальное время",max_value=50)
+    t1 = st.slider('Конечное время',min_value=5)
+    tlist = []
+    ylist = []
+    trr = float(t0)
+    res3 = res2.replace('tttttt', 't')
+    res4 = simplify(res3)
+    for gh in range(101):
+        sol = float(re(res4.subs(t,trr)))
+        tlist.append(trr)
+        ylist.append(sol)
+        trr = trr+((float(t1)-float(t0))/100)
+    for_graph = {'t':tlist,'y':ylist}
+    data = pd.DataFrame(for_graph)
+    graph_spec = altair.Chart(data).mark_line(interpolate="monotone").encode(
+        x = "t",
+        y = "y"
+    )
+    graph = st.altair_chart(graph_spec,use_container_width=True)
 
 
 
--- Mid2.py
+++ Mid2.py
@@ -1,43 +1,38 @@
 import sympy
 from sympy import Function, Eq, simplify, dsolve, Derivative, sin, cos, exp, checkodesol, Equality
-from sympy.abc import x
+from sympy.abc import x,tau,theta
 
-f = Function('f')
-func = str(input('function: ') or 'f')
-vari = str(input('variable: ') or 'x')
-lst = str(input('left part: ') or '0')
-rst = str(input('right part: ') or '0')
-#st = 'ddf(x) - df(x) + exp(x)'
-if func != 'f':
-    lst = lst.replace(func,'f')
-    rst = rst.replace(func, 'f')
-if vari != 'x':
-    lst = lst.replace(vari, 'x')
-    rst = rst.replace(vari,'x')
-lst = lst.replace('ddf(x)', 'f(x).diff(x,2)')
-lst = lst.replace('df(x)','f(x).diff(x)')
-rst = rst.replace('ddf(x)', 'f(x).diff(x,2)')
-rst = rst.replace('df(x)','f(x).diff(x)')
-print(lst, '=', rst)
-leq = simplify(lst)
-req = simplify(rst)
-eq1 = Eq(leq - req,0)
-result = dsolve(eq1,f(x))# Solve the ODE
-#print(result)
-#print(type(result))
-#print(type(lst))
-if hasattr(result,'lhs'):
-    endstr = str(result.lhs) + '=' + str(result.rhs)
-    endstr = endstr.replace('f', func)
-    endstr = endstr.replace('x', vari)
-    endstr = endstr.replace('e' + vari + 'p', 'exp')
-    endstr = endstr.replace('e' + func + 'p', 'exp')
-    print(endstr)
-else:
-    for i in range (0,len(result)):
+def solve_normally(func,vari,lst,rst,deg):
+    f = Function('f')
+    if func != 'f':
+        lst = lst.replace(func,'f')
+        rst = rst.replace(func, 'f')
+    if vari != 'x':
+        lst = lst.replace(vari, 'x')
+        rst = rst.replace(vari,'x')
+    lst = lst.replace('ddf(x)', 'f(x).diff(x,2)')
+    lst = lst.replace('df(x)','f(x).diff(x)')
+    rst = rst.replace('ddf(x)', 'f(x).diff(x,2)')
+    rst = rst.replace('df(x)','f(x).diff(x)')
+    leq = simplify(lst)
+    req = simplify(rst)
+    eq1 = Eq(leq - req,0)
+    if deg == 2:
+        result = dsolve(eq1,f(x),ics={f(0):tau, f(x).diff(x).subs(x,0):theta})
+    else:
+        result = dsolve(eq1, f(x), ics={f(0): tau})
+    if hasattr(result,'lhs'):
         endstr = str(result.lhs) + '=' + str(result.rhs)
         endstr = endstr.replace('f', func)
         endstr = endstr.replace('x', vari)
-        endstr = endstr.replace('e'+vari+'p','exp')
+        endstr = endstr.replace('e' + vari + 'p', 'exp')
         endstr = endstr.replace('e' + func + 'p', 'exp')
-        print(endstr)
+        return (endstr)
+    else:
+        for i in range (0,len(result)):
+            endstr = str(result.lhs) + '=' + str(result.rhs)
+            endstr = endstr.replace('f', func)
+            endstr = endstr.replace('x', vari)
+            endstr = endstr.replace('e'+vari+'p','exp')
+            endstr = endstr.replace('e' + func + 'p', 'exp')
+            return (endstr)
